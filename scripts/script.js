80c8d5cbc10b5f3fa82dd4c101e15caa183ee525
Nikcet <mstislav.kr@yandex.ru>
2021-09-18 00:11:34 +0300

    Написал скрипт
    Плюс сделал некоторые мелкие исправления

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+let doc = document.querySelector('.page');
+let popupWindow = doc.querySelector('.popup');
+let popupClose = doc.querySelector('.popup__close');
+
+let editButton = doc.querySelector('.profile__edit-button');
+let submitButton = doc.querySelector('.popup__submit-btn');
+
+let profileName = doc.querySelector('.profile__name');
+let profileDescription = doc.querySelector('.profile__description');
+
+let nameInput = doc.querySelector('.popup__name');
+let aboutInput = doc.querySelector('.popup__about');
+
+
+function openPopup() {
+    popupWindow.classList.add('popup_opened');
+}
+
+
+function setPopupFormFromProfile() {
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileDescription.textContent;
+}
+
+
+function closePopup() {
+    popupWindow.classList.remove('popup_opened');
+}
+
+
+function saveInfoFromPopup() {
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = aboutInput.value;
+}
+
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+    saveInfoFromPopup();
+    closePopup();
+}
+
+
+editButton.addEventListener('click', function () {
+    openPopup();
+    setPopupFormFromProfile();
+});
+popupClose.addEventListener('click', closePopup);
+
+submitButton.addEventListener('click', formSubmitHandler);
