7e8a7d017e72b4d5cf02f9e02ea3fdea03c6be4e
Nikcet <mstislav.kr@yandex.ru>
2021-11-03 20:23:09 +0300

    Исправления по замечаниям ревьюера
    1. Валидация валидирует только конкретную форму, а не все сразу;
    2. Объекты валидаторов создаются ровно один раз.

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -2,41 +2,39 @@ export class FormValidator {
     constructor(config, form) {
         this.config = config;
         this.form = form;
+        this.enableValidation();
     }
 
     // Запускает процесс валидации форм
-    enableValidation(config) {
-        const formList = Array.from(document.querySelectorAll(this.config.formSelector));
-        formList.forEach((formElement) => {
-            formElement.addEventListener('submit', (event) => {
-                event.preventDefault();
-            })
-            this._setEventListener(formElement, this.config);
-        });
+    enableValidation() {
+        this.form.addEventListener('submit', (event) => {
+            event.preventDefault();
+        })
+        this._setEventListener(this.form, this.config);
     }
 
     // Устанавливает слушатель события input на поле
-    _setEventListener = (form, config) => {
-        const inputList = Array.from(form.querySelectorAll(config.inputSelector));
-        const buttonElement = form.querySelector(config.submitButtonSelector);
+    _setEventListener = () => {
+        const inputList = Array.from(this.form.querySelectorAll(this.config.inputSelector));
+        const buttonElement = this.form.querySelector(this.config.submitButtonSelector);
 
-        this._toggleButtonState(inputList, buttonElement, config);
+        this._toggleButtonState(inputList, buttonElement, this.config);
         inputList.forEach((inputElement) => {
             inputElement.addEventListener('input', () => {
-                this._isValid(form, inputElement, config);
-                this._toggleButtonState(inputList, buttonElement, config);
+                this._isValid(this.form, inputElement, this.config);
+                this._toggleButtonState(inputList, buttonElement, this.config);
             })
         })
     };
 
     // Переключает кнопку из активного состояния в неактивное и обратно
-    _toggleButtonState = (inputList, button, config) => {
+    _toggleButtonState = (inputList, button) => {
         if (this._hasInvalidInput(inputList)) {
             button.disabled = true;
-            button.classList.add(config.inactiveButtonClass);
+            button.classList.add(this.config.inactiveButtonClass);
         } else {
             button.disabled = false;
-            button.classList.remove(config.inactiveButtonClass);
+            button.classList.remove(this.config.inactiveButtonClass);
         }
     }
 
@@ -48,27 +46,27 @@ export class FormValidator {
     };
 
     // Проверяет поле на валидность
-    _isValid = (form, inputElement, config) => {
+    _isValid = (inputElement) => {
         if (!inputElement.validity.valid) {
-            this._showInputError(form, inputElement, inputElement.validationMessage, config);
+            this._showInputError(this.form, inputElement, inputElement.validationMessage, this.config);
         } else {
-            this._hideInputError(form, inputElement, config);
+            this._hideInputError(this.form, inputElement, this.config);
         }
     };
 
 
     // Функция, которая добавляет класс с ошибкой
-    _showInputError = (form, element, errorMessage, config) => {
-        const errorElement = form.querySelector(`.${element.id}-error`);
-        element.classList.add(config.inputErrorClass);
+    _showInputError = (element, errorMessage) => {
+        const errorElement = this.form.querySelector(`.${element.id}-error`);
+        element.classList.add(this.config.inputErrorClass);
         errorElement.textContent = errorMessage;
         errorElement.classList.add(`${element.id}-error_active`);
     };
 
     // Функция, которая удаляет класс с ошибкой
-    _hideInputError = (form, element, config) => {
-        const errorElement = form.querySelector(`.${element.id}-error`);
-        element.classList.remove(config.inputErrorClass);
+    _hideInputError = (element) => {
+        const errorElement = this.form.querySelector(`.${element.id}-error`);
+        element.classList.remove(this.config.inputErrorClass);
         errorElement.classList.remove(`${element.id}-error_active`);
         errorElement.textContent = '';
     };
