4ca6dbd83b5dc9ead8dab6d8d4af63b95dd78aea
Nikcet <mstislav.kr@yandex.ru>
2021-11-19 03:08:59 +0300

    Класс открытия попапа для изображений

--- scripts/PopupWithImage.js
+++ scripts/PopupWithImage.js
@@ -1,14 +1,15 @@
 import Popup from './Popup.js';
+import { popupImageIdSelector, imagePopupSelector, descriptionPopupSelector } from './constants.js';
 
 class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
-        this.popupSelector = document.querySelector('#popupImage');
+        this.popupSelector = document.querySelector(popupImageIdSelector);
     }
 
     open(event) {
-        this._imagePopup = this.popupSelector.querySelector('.image-popup__image');
-        this._descrPopup = this.popupSelector.querySelector('.image-popup__description');
+        this._imagePopup = this.popupSelector.querySelector(imagePopupSelector);
+        this._descrPopup = this.popupSelector.querySelector(descriptionPopupSelector);
         this._imagePopup.src = event.target.src;
         this._imagePopup.alt = event.target.alt;
         this._descrPopup.textContent = event.target.alt;
--- scripts/constants.js
+++ scripts/constants.js
@@ -9,9 +9,33 @@ const popupAddCard = document.querySelector('#add-card');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
+const nameInput = document.querySelector('#popup__name');
+const descriptionInput = document.querySelector('#popup__about');
+
 const formSelector = '.popup__form';
 const inputSelector = '.popup__input';
 
 const KEY_CODE = 'Escape';
 
-export { elems, editButton, addButton, popupAddCard, popupEditProfile, KEY_CODE, inputSelector, formSelector, profileName, profileDescription };
+const popupImageIdSelector = '#popupImage';
+const imagePopupSelector = '.image-popup__image';
+const descriptionPopupSelector = '.image-popup__description';
+
+
+export {
+    elems,
+    editButton,
+    addButton,
+    popupAddCard,
+    popupEditProfile,
+    KEY_CODE,
+    inputSelector,
+    formSelector,
+    profileName,
+    profileDescription,
+    popupImageIdSelector,
+    imagePopupSelector,
+    descriptionPopupSelector,
+    nameInput,
+    descriptionInput
+};
