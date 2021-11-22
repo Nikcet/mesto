0bb18d3b138fe92de0beb10bb6c3ba661e7b40c8
Nikcet <mstislav.kr@yandex.ru>
2021-11-22 20:35:59 +0300

    Добавил строковые константы

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,10 +1,17 @@
+import {
+    heartQuerySelector,
+    deleteQuerySelector,
+    imageQuerySelector,
+    elementsImageQuerySelector,
+    elementsNameQuerySelector,
+} from './constants.js';
+
 class Card {
     constructor({ item, handleCardClick }, templateSelector) {
         this._item = item;
         this._name = item.name;
         this._link = item.link;
         this._templateSelector = templateSelector;
-        this.ESC_CODE = 'Escape';
         this.handleCardClick = handleCardClick;
     };
 
@@ -23,20 +30,20 @@ class Card {
         this._cardElement = this._getTemplate();
 
         // Заполняет шаблон данными
-        this._cardTemplateImage = this._cardElement.querySelector('.elements__image');
-        this._cardTemplateName = this._cardElement.querySelector('.elements__name');
+        this._cardTemplateImage = this._cardElement.querySelector(elementsImageQuerySelector);
+        this._cardTemplateName = this._cardElement.querySelector(elementsNameQuerySelector);
         this._cardTemplateImage.src = this._link;
         this._cardTemplateImage.alt = this._name;
         this._cardTemplateName.textContent = this._name;
 
         // Лайк
-        this._cardElement.querySelector('.elements__heart').addEventListener('click', this._setLike);
+        this._cardElement.querySelector(heartQuerySelector).addEventListener('click', this._setLike);
 
         // Удаление
-        this._cardElement.querySelector('.elements__delete').addEventListener('click', this._deleteCard);
+        this._cardElement.querySelector(deleteQuerySelector).addEventListener('click', this._deleteCard);
 
         // Попап изображения
-        this._cardElement.querySelector('.elements__image').addEventListener('click', () => this.handleCardClick(this._item));
+        this._cardElement.querySelector(imageQuerySelector).addEventListener('click', () => this.handleCardClick(this._item));
 
         return this._cardTemplate;
     }
