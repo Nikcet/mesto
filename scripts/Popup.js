f85b659cbc0885e650eb6f23f78879a74eb8f4da
Nikcet <mstislav.kr@yandex.ru>
2021-11-22 19:57:48 +0300

    Изменил логику работы класса:
    1. Передаю селектор попапа в виде константной переменной;
    2. Метод close теперь - стрелочный
    3. Слушатели событий добавляются только один раз. И не удаляются, кроме слушателя клавиши Esc
    4. Добавил пару комментариев

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -1,28 +1,35 @@
-import { KEY_CODE } from './constants.js';
+import { KEY_CODE, popupOpenedClassSelector, popupCloseQuerySelector } from './constants.js';
 
 export default class Popup {
     constructor(popupSelector) {
         this._currentPopup = popupSelector;
+        this._popupClose = this._currentPopup.querySelector(popupCloseQuerySelector);
     }
 
+    // Открывает попап
     open() {
-        this._currentPopup.classList.add('popup_opened');
+        this._currentPopup.classList.add(popupOpenedClassSelector);
         document.addEventListener('keydown', this._handleEscClose);
     }
 
-    close() {
-        this._currentPopup.classList.remove('popup_opened');
+    // Закрывает попап
+    close = () => {
+        this._currentPopup.classList.remove(popupOpenedClassSelector);
         document.removeEventListener('keydown', this._handleEscClose);
     }
 
+    // Вешает слушатели кликов по крестовине и оверлею на попап
     setEventListeners() {
-        this._currentPopup.querySelector('.popup__close').addEventListener('click', () => this.close());
-
-        this._currentPopup.addEventListener('click', event => {
-            if (event.target.classList.contains('popup_opened')) this.close();
-        })
+        this._popupClose.addEventListener('click', this.close);
+        this._currentPopup.addEventListener('click', this._closePopup);
     }
 
+    // Закрывает попап по клику на оверлей
+    _closePopup = (event) => {
+        if (event.target.classList.contains(popupOpenedClassSelector)) this.close();
+    }
+
+    // Закрывает попап ко нажатию на клавишу Esc
     _handleEscClose = (event) => {
         if (event.key === KEY_CODE) this.close();
     }
