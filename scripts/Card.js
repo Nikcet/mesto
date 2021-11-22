a036dbbc9b4239eb7a8a5d49d048159cb9bacf14
Nikcet <mstislav.kr@yandex.ru>
2021-11-22 20:01:52 +0300

    Преобразование класса, еще в процессе:
    1. Удалил некоторые лишние функции
    2. Удалил мусорный код
    3. Добавил метод _gettemplate
    4. Реорганизовал метод createCard
    5. Экспорт класса по-умолчанию

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,38 +1,42 @@
-export class Card {
-    constructor(item, templateSelector) {
+class Card {
+    constructor({ item, handleCardClick }, templateSelector) {
+        this._item = item;
         this._name = item.name;
-        this._image = item.link;
+        this._link = item.link;
         this._templateSelector = templateSelector;
         this.ESC_CODE = 'Escape';
+        this.handleCardClick = handleCardClick;
     };
-    // Возвращает полностью готовую карточку
-    addCard = () => {
-        this._card = this._createCard();
-        return this._card;
-    };
-    // Собирает карточку
-    _createCard = () => {
-        // Копирует шаблон разметки
-        this._cardTemplate = this._templateSelector
+
+    // Возвращает пустой шаблон
+    _getTemplate = () => {
+        return this._cardTemplate = this._templateSelector
             .querySelector('.elements__template')
             .content
+            .querySelector('.elements__card')
             .cloneNode(true);
+    };
+
+    // Возвращает готовую карточку
+    createCard = () => {
+        // Копирует шаблон разметки
+        this._cardElement = this._getTemplate();
 
         // Заполняет шаблон данными
-        this._cardTemplateImage = this._cardTemplate.querySelector('.elements__image');
-        this._cardTemplateName = this._cardTemplate.querySelector('.elements__name');
-        this._cardTemplateImage.src = this._image;
+        this._cardTemplateImage = this._cardElement.querySelector('.elements__image');
+        this._cardTemplateName = this._cardElement.querySelector('.elements__name');
+        this._cardTemplateImage.src = this._link;
         this._cardTemplateImage.alt = this._name;
         this._cardTemplateName.textContent = this._name;
 
         // Лайк
-        this._cardTemplate.querySelector('.elements__heart').addEventListener('click', this._setLike);
+        this._cardElement.querySelector('.elements__heart').addEventListener('click', this._setLike);
 
         // Удаление
-        this._cardTemplate.querySelector('.elements__delete').addEventListener('click', this._deleteCard);
+        this._cardElement.querySelector('.elements__delete').addEventListener('click', this._deleteCard);
 
         // Попап изображения
-        this._cardTemplate.querySelector('.elements__image').addEventListener('click', this._openImagePopup);
+        this._cardElement.querySelector('.elements__image').addEventListener('click', () => this.handleCardClick(this._item));
 
         return this._cardTemplate;
     }
@@ -45,33 +49,6 @@ export class Card {
         event.target.closest('.elements__card').remove();
     }
 
-    // Открывает попап с изображением
-    _openImagePopup = (event) => {
-        this._popupImage = document.querySelector('#popupImage');
-        this._imagePopup = this._popupImage.querySelector('.image-popup__image');
-        this._descrPopup = this._popupImage.querySelector('.image-popup__description');
-        this._imagePopup.src = event.target.src;
-        this._imagePopup.alt = event.target.alt;
-        this._descrPopup.textContent = event.target.alt;
-
-        this._openPopup(this._popupImage);
-    };
-    // Открывает попап изображения карточки
-    _openPopup = (popup) => {
-        popup.classList.add('popup_opened');
-        document.addEventListener('keydown', this._closeByEsc);
-    }
-    // Закрывает попап изображения карточки
-    _closePopup = (popup) => {
-        popup.classList.remove('popup_opened');
-        document.removeEventListener('keydown', this._closeByEsc);
-    }
-    // Закрывает попап изображения карточки по нажатию на клавишу Esc
-    _closeByEsc = (event) => {
-        if (event.key === this.ESC_CODE) {
-            this._openedPopup = document.querySelector('.popup_opened');
-            this._closePopup(this._openedPopup);
-        }
-    }
-
 }
+
+export default Card;
