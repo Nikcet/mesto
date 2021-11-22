8a472caca059a9c1d4b1c550609d293deed5e67f
Nikcet <mstislav.kr@yandex.ru>
2021-11-22 19:51:40 +0300

    Класс работает с попап-формами

---
+++ scripts/PopupWithForm.js
@@ -0,0 +1,35 @@
+import Popup from './Popup.js';
+import { inputQuerySelector, formQuerySelector } from './constants.js';
+
+export default class PopupWithForm extends Popup {
+    constructor(popupSelector, submitHandler) {
+        super(popupSelector);
+        this._submitHandler = submitHandler;
+        this._form = popupSelector.querySelector(formQuerySelector);
+        this._inputList = popupSelector.querySelectorAll(inputQuerySelector);
+        this._inputValues = {};
+    }
+
+    _getInputValues = () => {
+        this._inputList.forEach(item => {
+            if (item.value.length > 0) {
+                this._inputValues[item.name] = item.value;
+            }
+        })
+        return this._inputValues;
+    }
+
+    close() {
+        this._form.reset();
+        super.close();
+    }
+
+    setEventListeners = () => {
+        super.setEventListeners();
+        this._form.addEventListener('submit', event => {
+            event.preventDefault();
+            this._submitHandler(this._getInputValues());
+            this.close();
+        })
+    }
+}
