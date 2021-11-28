e1cf3b89f24ea4d014ef9cde3dc8144d76904ff5
Nikcet <mstislav.kr@yandex.ru>
2021-11-28 20:45:15 +0300

    Добавил констант

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -2,6 +2,8 @@ const elems = document.querySelector('.elements');
 
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
+const editAvatarButton = document.querySelector('.profile__avatar-edit-btn');
+const agreementButton = document.querySelector('#popup__delete');
 
 const popupEditProfile = document.querySelector('#edit-prof');
 const popupAddCard = document.querySelector('#add-card');
@@ -14,6 +16,9 @@ const nameInput = document.querySelector('#popup__name');
 const descriptionInput = document.querySelector('#popup__about');
 
 const popupImage = document.querySelector('#popupImage')
+
+const popupQuestion = document.querySelector('#popupQuestion');
+const popupAvatar = document.querySelector('#popupAvatar');
 
 const formQuerySelector = '.popup__form';
 const inputQuerySelector = '.popup__input';
@@ -30,6 +35,7 @@ const descriptionPopupSelector = '.image-popup__description';
 const heartQuerySelector = '.elements__heart';
 const deleteQuerySelector = '.elements__delete';
 const imageQuerySelector = '.elements__image';
+const heartNumberQuerySelector = '.elements__heart-number';
 
 const elementsImageQuerySelector = '.elements__image';
 const elementsNameQuerySelector = '.elements__name';
@@ -39,6 +45,7 @@ export {
     elems,
     editButton,
     addButton,
+    agreementButton,
     popupAddCard,
     popupEditProfile,
     KEY_CODE,
@@ -57,7 +64,11 @@ export {
     heartQuerySelector,
     deleteQuerySelector,
     imageQuerySelector,
+    heartNumberQuerySelector,
     elementsImageQuerySelector,
     elementsNameQuerySelector,
     popupImage,
+    popupQuestion,
+    popupAvatar,
+    editAvatarButton,
 };
